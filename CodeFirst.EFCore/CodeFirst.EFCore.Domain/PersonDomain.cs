cb4b29cb38a449ca7559c5957e49040c55c97e1a
16R137 <igriet.gonzalez@outlook.com>
2019-04-17 23:19:46 -0500

    creacion de controller y para poder completar el ejemplo para esto se creo una capa de domain donde se llama al contexto usando EFCore ademas para usar el contexto este se va pidiendo como parametro en el constructor del controller y del domain

---
+++ CodeFirst.EFCore/CodeFirst.EFCore.Domain/PersonDomain.cs
@@ -0,0 +1,25 @@
+﻿using CodeFirst.EFCore.Data;
+using CodeFirst.EFCore.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeFirst.EFCore.Domain
+{
+    public class PersonDomain
+    {
+        private PersonDbContext _context;
+
+        public PersonDomain(PersonDbContext context)
+        {
+            _context = context;
+        }
+
+        public void AddPerson(Person person)
+        {
+            person.PersonId = Guid.NewGuid();
+            _context.Set<Person>().Add(person);
+            _context.SaveChanges();
+        }
+    }
+}
---
+++ CodeFirst.EFCore/CodeFirst.EFCore/Controllers/PersonController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CodeFirst.EFCore.Data;
+using CodeFirst.EFCore.Domain;
+using CodeFirst.EFCore.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CodeFirst.EFCore.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Person")]
+    public class PersonController : Controller
+    {
+        private PersonDbContext _context;
+
+        public PersonController(PersonDbContext context)
+        {
+            _context = context;
+        }
+        // GET: api/Person
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/Person/5
+        [HttpGet("{id}", Name = "Get")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Person
+        [HttpPost]
+        public void Post([FromBody]Person value)
+        {
+            var domain = new PersonDomain(_context);
+            domain.AddPerson(value);
+        }
+        
+        // PUT: api/Person/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+        
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
